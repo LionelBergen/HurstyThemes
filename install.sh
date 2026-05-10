06cbfcc1d3b6fe62e61b15c2ed2f4bd0b25e7040
Lionel Bergen <LionelBergen@live.com>
2026-05-10 04:50:28 -0500

    done editing install.sh

--- install.sh
+++ install.sh
@@ -34,10 +34,34 @@ else
     echo "made dir: $retropie_scripts_dir"
 fi
 
-cp /tmp/hursty/themerandom.sh $retropie_scripts_dir
-chmod 777 $retropie_scripts_dir/themerandom.sh
+cp /tmp/hursty/themerandom.sh /home/pi/scripts
+chmod 777 /home/pi/scripts/themerandom.sh
 
 # Update RetroPie gamelist.xml to add new entry
+cp /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml.bkp
+cp /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml /tmp
+
+
+cat /tmp/gamelist.xml |grep -v "</gameList>" > /tmp/templist.xml
+
+ifexist=`cat /tmp/templist.xml |grep hurstythemes |wc -l`
+
+if [[ ${ifexist} > 0 ]]; then
+  echo "already in gamelist.xml" > /tmp/exists
+else
+  echo "	<game>" >> /tmp/templist.xml
+  echo "		<path>./hurstythemes.sh</path>" >> /tmp/templist.xml
+  echo "		<name>Hursty's Themes</name>" >> /tmp/templist.xml
+  echo "		<desc>Install, uninstall, or update RetroHursty69's EmulationStation themes. It also includes an ES bootup theme randomizer to automatically switch themes on bootup.</desc>" >> /tmp/templist.xml
+  echo "		<image>./icons/hurstythemes.png</image>" >> /tmp/templist.xml
+  echo "		<playcount>1</playcount>" >> /tmp/templist.xml
+  echo "		<lastplayed></lastplayed>" >> /tmp/templist.xml
+  echo "	</game>" >> /tmp/templist.xml
+  echo "</gameList>" >> /tmp/templist.xml
+  cp /tmp/templist.xml /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+fi
+
+echo "DONE!"
 
 exit 1
 
