956b0612084e31c88e1d3b47a8f48a9277c8dff8
Lionel Bergen <LionelBergen@live.com>
2026-05-10 04:44:49 -0500

    more testing

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@
 
 # Download repository
 
-retropie_folder="/home/retrogaming/RetroPie"
+retropie_dir="/home/retrogaming/RetroPie"
+
+retropie_menu_dir=$retropie_dir/retropiemenu
+retropie_scripts_dir=$retropie_dir/scripts
 
 rm -rf "/tmp/hursty"
 mkdir -p "/tmp/hursty"
@@ -17,10 +20,24 @@ git clone "https://github.com/RetroHursty69/HurstyThemes.git" "/tmp/hursty"
 
 # Move files to proper directory
 
-cp /tmp/hursty/hurstythemes.sh $retropie_folder/retropiemenu
+cp /tmp/hursty/hurstythemes.sh $retropie_menu_dir
 
-echo "done test"
+echo "copied hurstythemes.sh"
+
+chmod 777 $retropie_menu_dir/hurstythemes.sh
+
+cp /tmp/hursty/hurstythemes.png $retropie_menu_dir/icons
+if [[ -d "$retropie_scripts_dir" ]]; then
+  echo "scripts dir exists" > /dev/null
+else
+    mkdir $retropie_scripts_dir
+    echo "made dir: $retropie_scripts_dir"
+fi
+
+cp /tmp/hursty/themerandom.sh $retropie_scripts_dir
+chmod 777 $retropie_scripts_dir/themerandom.sh
+
+# Update RetroPie gamelist.xml to add new entry
 
 exit 1
-fi
 
