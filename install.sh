95c0e46d08f89acf5f85572a900cf03590d8b9c4
Lionel Bergen <LionelBergen@live.com>
2026-05-10 04:24:36 -0500

    testing

--- install.sh
+++ install.sh
@@ -9,43 +9,18 @@
 
 # Download repository
 
+retropie_folder="/home/retrogaming/RetroPie"
+
 rm -rf "/tmp/hursty"
 mkdir -p "/tmp/hursty"
 git clone "https://github.com/RetroHursty69/HurstyThemes.git" "/tmp/hursty"
 
 # Move files to proper directory
 
-cp /tmp/hursty/hurstythemes.sh /home/pi/RetroPie/retropiemenu
-chmod 777 /home/pi/RetroPie/retropiemenu/hurstythemes.sh
-cp /tmp/hursty/hurstythemes.png /home/pi/RetroPie/retropiemenu/icons
-if [[ -d "/home/pi/scripts" ]]; then
-echo "exists" > /dev/null
-else
-    mkdir /home/pi/scripts
-fi
-cp /tmp/hursty/themerandom.sh /home/pi/scripts
-chmod 777 /home/pi/scripts/themerandom.sh
+cp /tmp/hursty/hurstythemes.sh $retropie_folder/retropiemenu
 
-# Update RetroPie gamelist.xml to add new entry
+echo "done test"
 
-cp /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml.bkp
-cp /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml /tmp
-
-cat /tmp/gamelist.xml |grep -v "</gameList>" > /tmp/templist.xml
-
-ifexist=`cat /tmp/templist.xml |grep hurstythemes |wc -l`
-if [[ ${ifexist} > 0 ]]; then
-  echo "already in gamelist.xml" > /tmp/exists
-else
-  echo "	<game>" >> /tmp/templist.xml
-  echo "		<path>./hurstythemes.sh</path>" >> /tmp/templist.xml
-  echo "		<name>Hursty's Themes</name>" >> /tmp/templist.xml
-  echo "		<desc>Install, uninstall, or update RetroHursty69's EmulationStation themes. It also includes an ES bootup theme randomizer to automatically switch themes on bootup.</desc>" >> /tmp/templist.xml
-  echo "		<image>./icons/hurstythemes.png</image>" >> /tmp/templist.xml
-  echo "		<playcount>1</playcount>" >> /tmp/templist.xml
-  echo "		<lastplayed></lastplayed>" >> /tmp/templist.xml
-  echo "	</game>" >> /tmp/templist.xml
-  echo "</gameList>" >> /tmp/templist.xml
-  cp /tmp/templist.xml /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+exit 1
 fi
 
